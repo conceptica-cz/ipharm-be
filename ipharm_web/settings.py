805824989f7ce8413d96e14dbca99b3d65151d6f
Alexey Londkevich <londkevich@gmail.com>
2021-09-30 20:12:55 +0200

    Remove secret key from settings.

--- ipharm_web/settings.py
+++ ipharm_web/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c163ovoq&l=4f!_y16)o(&srj&d(77x81s@@7lxd83_@m2eoon'
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
