9131a81c2b50ca657b23a542092848a9a139682a
Alexey Londkevich <londkevich@gmail.com>
2021-10-16 11:17:20 +0200

    Add github CI.

--- ipharm/apps.py
+++ ipharm/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class IpharmConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ipharm'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "ipharm"
--- ipharm/urls.py
+++ ipharm/urls.py
@@ -4,7 +4,13 @@ from .views import patients
 
 urlpatterns = [
     path("clinics/", patients.ClinicListView.as_view(), name="clinic_list"),
-    path("clinics/<int:pk>/", patients.ClinicDetailView.as_view(), name="clinic_detail"),
+    path(
+        "clinics/<int:pk>/", patients.ClinicDetailView.as_view(), name="clinic_detail"
+    ),
     path("patients/", patients.PatientListView.as_view(), name="patient_list"),
-    path("patients/<int:pk>/", patients.PatientDetailView.as_view(), name="patient_detail"),
+    path(
+        "patients/<int:pk>/",
+        patients.PatientDetailView.as_view(),
+        name="patient_detail",
+    ),
 ]
