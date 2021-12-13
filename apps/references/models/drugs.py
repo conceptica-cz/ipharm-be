dd682751d5392cf6d9dde4ac5af5e20bb8d1d962
Alexey Londkevich <londkevich@gmail.com>
2021-12-13 21:19:56 +0100

    Update Drug fields

--- apps/references/models/drugs.py
+++ apps/references/models/drugs.py
@@ -3,8 +3,12 @@ from updates.models import BaseUpdatableModel
 
 
 class Drug(BaseUpdatableModel):
-    code_sukl = models.CharField(max_length=50, unique=True)
-    name = models.CharField(max_length=255)
+    code_sukl = models.CharField(max_length=50, unique=True, help_text="Kód SÚKL")
+    name = models.CharField(max_length=255, help_text="Název léku")
+    name_supplement = models.CharField(
+        max_length=255, blank=True, help_text="Doplněk názvu"
+    )
+    atc_group = models.CharField(max_length=255, blank=True, help_text="ATC skupina")
 
     def __str__(self):
         return self.name
