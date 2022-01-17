8774c0759bd05549b9294312cf49f32a36ed29fe
Alexey Londkevich <londkevich@gmail.com>
2022-01-17 19:11:04 +0100

    Add handling zero dekuzWho

--- tests/updates/bulovka/test_transformers.py
+++ tests/updates/bulovka/test_transformers.py
@@ -151,3 +151,55 @@ class PatientTransformerTest(TestCase):
         output_data = patient_transformer(input_data)
 
         self.assertEqual(output_data, expected)
+
+    def test_patient_transformer_if_dekurzwho_is_zero(self):
+        """
+        dekurzWho = 0 should be transformed to None
+        """
+        input_data = {
+            "patientId": 42,
+            "name": "Doe John",
+            "birthNumber": "1234567890",
+            "birthDate": "1987-04-02",
+            "insuranceCompany": "111",
+            "insuranceNumber": "123456789",
+            "height": 173.0,
+            "weight": 61.0,
+            "diagnosis": "U071",
+            "hospitalizationId": 43,
+            "departmentIn": 600,
+            "dateIn": "2021-12-26 14:27",
+            "dekurzTime": "2022-01-06 09:30",
+            "dekurzWho": 0,
+            "dekurzDepartment": 600,
+        }
+
+        expected = {
+            "patient": {
+                "external_id": 42,
+                "first_name": "John",
+                "last_name": "Doe",
+                "birth_number": "1234567890",
+                "birth_date": "1987-04-02",
+                "insurance_company": "111",
+                "insurance_number": "123456789",
+                "height": 173.0,
+                "weight": 61.0,
+            },
+            "care": {
+                "external_id": 43,
+                "department": 600,
+                "started_at": "2021-12-26 14:27",
+                "finished_at": None,
+                "main_diagnosis": "U071",
+            },
+            "dekurz": {
+                "made_at": "2022-01-06 09:30",
+                "doctor": None,
+                "department": 600,
+            },
+        }
+
+        output_data = patient_transformer(input_data)
+
+        self.assertEqual(output_data, expected)
