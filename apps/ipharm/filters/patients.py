1edbf378c82db8c4bdd39fa18d4d1ddabad3044b
Alexey Londkevich <londkevich@gmail.com>
2022-02-11 14:14:05 +0100

    Add new PatientFilters

--- apps/ipharm/filters/patients.py
+++ apps/ipharm/filters/patients.py
@@ -7,6 +7,20 @@ class PatientFilter(django_filters.FilterSet):
 
     care_type = django_filters.CharFilter(field_name="current_care__care_type")
 
+    has_checkin = django_filters.BooleanFilter(
+        field_name="current_care__checkin", lookup_expr="isnull", exclude=True
+    )
+
+    risk_level = django_filters.CharFilter(
+        field_name="current_care__checkin__risk_level", lookup_expr="exact"
+    )
+
+    has_notification_datetime = django_filters.BooleanFilter(
+        field_name="current_care__pharmacologicalplan__notification_datetime",
+        lookup_expr="isnull",
+        exclude=True,
+    )
+
     class Meta:
         model = Patient
         fields = {
--- tests/ipharm/filters/test_patients.py
+++ tests/ipharm/filters/test_patients.py
@@ -2,7 +2,12 @@ from django.test import TestCase
 from ipharm.filters import PatientFilter
 from ipharm.models import Care, Patient
 
-from factories.ipharm import CareFactory, PatientFactory
+from factories.ipharm import (
+    CareFactory,
+    CheckInFactory,
+    PatientFactory,
+    PharmacologicalPlanFactory,
+)
 from factories.references import ClinicFactory
 
 
@@ -86,6 +91,111 @@ class PatientFilterTest(TestCase):
             [
                 self.patient_1_hospital_1,
                 self.patient_2_hospital_1,
+            ],
+            transform=lambda x: x,
+            ordered=False,
+        )
+
+
+class PatientFilterByCheckInTest(TestCase):
+    def setUp(self) -> None:
+        self.patient_without_checkin = PatientFactory()
+
+        self.patient_with_risk_level_1 = PatientFactory()
+        care = CareFactory(
+            patient=self.patient_with_risk_level_1,
+        )
+        CheckInFactory(
+            care=care,
+            risk_level=1,
+        )
+
+        self.patient_with_risk_level_3 = PatientFactory()
+        care = CareFactory(
+            patient=self.patient_with_risk_level_3,
+        )
+        CheckInFactory(
+            care=care,
+            risk_level=3,
+        )
+
+        self.patient_without_notification_datetime = PatientFactory()
+        care = CareFactory(
+            patient=self.patient_without_notification_datetime,
+        )
+        CheckInFactory(
+            care=care,
+            risk_level=2,
+        )
+        PharmacologicalPlanFactory(care=care, notification_datetime=None)
+
+        self.patient_with_notification_datetime = PatientFactory()
+        care = CareFactory(
+            patient=self.patient_with_notification_datetime,
+        )
+        CheckInFactory(
+            care=care,
+            risk_level=2,
+        )
+        PharmacologicalPlanFactory(care=care)
+
+    def test_has_checkin_filter(self):
+        f = PatientFilter(data={"has_checkin": True})
+        queryset = f.qs
+        self.assertEqual(queryset.count(), 4)
+        self.assertQuerysetEqual(
+            queryset,
+            [
+                self.patient_with_risk_level_1,
+                self.patient_with_risk_level_3,
+                self.patient_without_notification_datetime,
+                self.patient_with_notification_datetime,
+            ],
+            transform=lambda x: x,
+            ordered=False,
+        )
+
+        f = PatientFilter(data={"has_checkin": False})
+        queryset = f.qs
+        self.assertQuerysetEqual(
+            queryset,
+            [
+                self.patient_without_checkin,
+            ],
+            transform=lambda x: x,
+            ordered=False,
+        )
+
+    def test_risk_level_filter(self):
+        f = PatientFilter(data={"risk_level": 1})
+        queryset = f.qs
+        self.assertQuerysetEqual(
+            queryset,
+            [
+                self.patient_with_risk_level_1,
+            ],
+            transform=lambda x: x,
+            ordered=False,
+        )
+
+        f = PatientFilter(data={"risk_level": 3})
+        queryset = f.qs
+        self.assertQuerysetEqual(
+            queryset,
+            [
+                self.patient_with_risk_level_3,
+            ],
+            transform=lambda x: x,
+            ordered=False,
+        )
+
+    def test_has_notification_datetime_filter(self):
+        f = PatientFilter(data={"has_notification_datetime": True})
+        queryset = f.qs
+        self.assertQuerysetEqual(
+            queryset,
+            [
+                self.patient_with_notification_datetime,
             ],
             transform=lambda x: x,
             ordered=False,
