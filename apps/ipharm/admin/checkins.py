39b8fd2ea3bc7fc28a9b39efbdfeec162f7b064e
Alexey Londkevich <londkevich@gmail.com>
2022-04-08 08:53:08 +0200

    Add django-debug-toolbar

--- apps/ipharm/admin/checkins.py
+++ apps/ipharm/admin/checkins.py
@@ -6,7 +6,7 @@ from ipharm.models import checkins
 class CheckIn_drugsInline(admin.TabularInline):
     model = checkins.CheckIn_drugs
     extra = 0
-    exclude = ["is_deleted"]
+    autocomplete_fields = ["drug"]
 
     def has_delete_permission(self, request, obj=None):
         return False
@@ -16,6 +16,7 @@ class CheckIn_high_interaction_potential_drugsInline(admin.TabularInline):
     model = checkins.CheckIn_high_interaction_potential_drugs
     extra = 0
     exclude = ["is_deleted"]
+    autocomplete_fields = ["drug"]
 
     def has_delete_permission(self, request, obj=None):
         return False
@@ -25,6 +26,7 @@ class CheckIn_diagnosesInline(admin.TabularInline):
     model = checkins.CheckIn_diagnoses
     extra = 0
     exclude = ["is_deleted"]
+    autocomplete_fields = ["diagnosis"]
 
     def has_delete_permission(self, request, obj=None):
         return False
@@ -34,6 +36,7 @@ class CheckIn_diagnoses_drugsInline(admin.TabularInline):
     model = checkins.CheckIn_diagnoses_drugs
     extra = 0
     exclude = ["is_deleted"]
+    autocomplete_fields = ["drug"]
 
     def has_delete_permission(self, request, obj=None):
         return False
@@ -43,6 +46,11 @@ class CheckIn_narrow_therapeutic_window_drugsInline(admin.TabularInline):
     model = checkins.CheckIn_narrow_therapeutic_window_drugs
     extra = 0
     exclude = ["is_deleted"]
+    autocomplete_fields = ["drug"]
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.select_related("drug")
 
     def has_delete_permission(self, request, obj=None):
         return False
@@ -51,7 +59,7 @@ class CheckIn_narrow_therapeutic_window_drugsInline(admin.TabularInline):
 @admin.register(checkins.CheckIn)
 class CheckInAdmin(BaseHistoryAdmin):
     list_display = ("id", "patient", "medical_procedure", "created_at", "updated_at")
-    list_select_related = ("care", "care__patient")
+    list_select_related = ("care", "care__patient", "medical_procedure")
     list_filter = ("care__patient__insurance_company", "medical_procedure")
     search_fields = ("care__patient__birth_number", "care__patient__last_name")
 
