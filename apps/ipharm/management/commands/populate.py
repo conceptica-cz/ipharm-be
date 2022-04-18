a1145bc807846c1070657f01a6aa51f3637fb556
Alexey Londkevich <londkevich@gmail.com>
2022-04-18 17:52:06 +0200

    Add new statistical reports.

--- apps/ipharm/management/commands/populate.py
+++ apps/ipharm/management/commands/populate.py
@@ -46,7 +46,7 @@ class Command(BaseCommand):
         for i in range(50):
             TagFactory()
             AdverseEffectFactory()
-        for i in range(200):
+        for i in range(101):
             care = CareFactory()
             print(f"Patient {care.patient} created")
             if random.randint(0, 1):
@@ -58,7 +58,10 @@ class Command(BaseCommand):
                     narrow_therapeutic_window_drugs__add=True,
                 )
                 if random.randint(0, 1):
-                    pharmacological_plan = PharmacologicalPlanFactory(care=care)
+                    pharmacological_plan = PharmacologicalPlanFactory(
+                        care=care,
+                        tags__add=True,
+                    )
                     [
                         PharmacologicalPlanCommentFactory(
                             pharmacological_plan=pharmacological_plan,
@@ -73,7 +76,10 @@ class Command(BaseCommand):
                         )
                         for _ in range(random.randint(0, 5))
                     ]
-                    RiskDrugHistoryFactory(care=care)
+                    RiskDrugHistoryFactory(
+                        care=care,
+                        tags__add=True,
+                    )
                     for _ in range(random.randint(1, 4)):
                         patient_information = PatientInformationFactory(care=care)
                         for i in range(random.randint(1, 4)):
@@ -82,7 +88,7 @@ class Command(BaseCommand):
                             patient_information.set_history_user(random.choice(users))
 
                     [
-                        PharmacologicalEvaluationFactory(care=care)
+                        PharmacologicalEvaluationFactory(care=care, tags__add=True)
                         for _ in range(random.randint(1, 5))
                     ]
 
