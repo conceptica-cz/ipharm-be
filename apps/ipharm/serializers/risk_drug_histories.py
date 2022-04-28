399472117dc82ca08672f20a01fcdaea92b09a77
Alexey Londkevich <londkevich@gmail.com>
2022-04-28 10:22:27 +0200

    Add nested diagnosis to RiskDrugHistoryListView.

--- apps/ipharm/serializers/risk_drug_histories.py
+++ apps/ipharm/serializers/risk_drug_histories.py
@@ -38,7 +38,7 @@ class RiskDrugHistorySerializer(serializers.ModelSerializer):
 
 class RiskDrugHistoryNestedSerializer(serializers.ModelSerializer):
     risk_drugs = DrugSerializer(many=True, read_only=True)
-    risk_risk_diagnoses = DiagnosisSerializer(many=True, read_only=True)
+    risk_diagnoses = DiagnosisSerializer(many=True, read_only=True)
     tags = TagSerializer(many=True, read_only=True)
     comments = RiskDrugHistoryCommentSerializer(many=True, read_only=True)
 
